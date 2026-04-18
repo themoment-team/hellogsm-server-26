27828aef51d56d31bee2fcaaa4c5c1edf7ea859a
hongjm0912~ <s25036@gsm.hs.kr>
2026-04-19 01:46:11 +0900

    fix(oneseo): 임시저장 시 achievement1_1 필드 누락 수정

--- src/main/java/team/themoment/hellogsmv3/domain/oneseo/dto/response/MiddleSchoolAchievementResDto.java
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/dto/response/MiddleSchoolAchievementResDto.java
@@ -6,7 +6,7 @@ import java.util.List;
 import lombok.Builder;
 
 @Builder
-public record MiddleSchoolAchievementResDto(List<Integer> achievement1_2, List<Integer> achievement2_1,
+public record MiddleSchoolAchievementResDto(List<Integer> achievement1_1, List<Integer> achievement1_2, List<Integer> achievement2_1,
         List<Integer> achievement2_2, List<Integer> achievement3_1, List<Integer> achievement3_2,
         List<String> generalSubjects, List<String> newSubjects, List<Integer> artsPhysicalAchievement,
         List<String> artsPhysicalSubjects, List<Integer> absentDays, Integer absentDaysCount,
