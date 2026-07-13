29f2d0f69a6dc76f3cb79534dc9f5e933a0c1eb8
snowykte0426 <snowykte0426@naver.com>
2026-07-13 20:05:11 +0900

    ci/cd(global): dev 네이밍 stage로 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,8 @@ DG_NAME=$(echo $DEPLOYMENT_GROUP_NAME)
 
 # 배포 그룹에 따라 다른 배포 스크립트를 실행합니다.
 if [ "$DG_NAME" == "api-stage-hellogsm-kr" ]; then
-  chmod +x /home/ec2-user/builds/scripts/dev-deploy.sh
-  /home/ec2-user/builds/scripts/dev-deploy.sh
+  chmod +x /home/ec2-user/builds/scripts/stage-deploy.sh
+  /home/ec2-user/builds/scripts/stage-deploy.sh
 
 elif [ "$DG_NAME" == "api-prod-hellogsm-kr" ]; then
   chmod +x /home/ec2-user/builds/scripts/prod-deploy.sh
